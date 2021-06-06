a7932b43bb94b31a14c1cff173c4c641e2e5e2e0
luke-h1 <luke.howsam@yahoo.com>
2021-06-06 18:41:17 +0100

    cleanup project

--- src/server/src/entities/Post.ts
+++ src/server/src/entities/Post.ts
@@ -1,12 +1,9 @@
-import { Field, Int } from 'type-graphql';
 import { BaseEntity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 export class Post extends BaseEntity {
-  @Field(() => Int)
   @PrimaryGeneratedColumn()
   id!: number;
 
-  @Field()
   @Column()
   pictureUrl: string;
 }
