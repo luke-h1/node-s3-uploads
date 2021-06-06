a9dfa900bdebdc71dad1b9f04f513d7a75efba27
luke-h1 <luke.howsam@yahoo.com>
2021-06-06 16:31:10 +0100

    get server running

---
+++ src/server/src/entities/Post.ts
@@ -0,0 +1,16 @@
+import { Field, Int } from 'type-graphql';
+import { BaseEntity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+export class Post extends BaseEntity {
+    @Field(() => Int)
+    @PrimaryGeneratedColumn()
+    id!: number;
+
+    @Field()
+    @Column()
+    name: string;
+
+    @Field()
+    @Column()
+    pictureUrl: string;
+}
---
+++ src/server/src/index.ts
@@ -0,0 +1,51 @@
+import 'reflect-metadata';
+import 'dotenv-safe/config';
+import express from 'express';
+import path from 'path';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
+import { createConnection } from 'typeorm';
+import cors from 'cors';
+import { __prod__ } from './constants';
+import { Post } from './entities/Post';
+import { HelloResolver } from './resolvers/HelloResolver';
+
+const main = async () => {
+  await createConnection({
+    type: 'postgres',
+    url: process.env.DATABASE_URL,
+    logging: !__prod__,
+    migrations: [path.join(__dirname, './migrations/*')],
+    synchronize: !__prod__,
+    entities: [Post],
+  });
+  // await conn.runMigrations();
+
+  const app = express();
+
+  app.set('trust proxy', 1); // Let Express know about nginx proxies
+  app.use(
+    cors({
+      origin: process.env.CORS_ORIGIN!,
+      credentials: true,
+    }),
+  );
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+    context: ({ req, res }) => ({
+      req,
+      res,
+    }),
+  });
+  apolloServer.applyMiddleware({ app, cors: false });
+  app.listen(process.env.PORT!, () => {
+    console.log(`Server running on http://localhost:${process.env.PORT} 🚀`);
+  });
+};
+main().catch((e) => {
+  console.error(e);
+});
